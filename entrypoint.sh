ac1e2434965b31057891a02cb473b20b3b4adbfc
bernardino de aguas <bernardino.deaguas@gmail.com>
2026-01-26 11:32:59 -0500

    se ejecuta dentro de entrypoint.sh Ahora ejecuta alembic upgrade head

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,10 @@ set -e
 
 echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] Starting application..."
 
+# Run database migrations
+echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] Running database migrations..."
+python -m alembic upgrade head
+
+echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] Migrations completed successfully"
 
 exec "$@"
