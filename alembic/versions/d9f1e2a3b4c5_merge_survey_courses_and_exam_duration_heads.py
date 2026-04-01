6fbe8caaa074c4234cc7d823e07e777400764d1d
bernardino de aguas <bernardino.deaguas@gmail.com>
2026-04-01 15:34:24 -0500

    se agrega solucionan problemas de migracion

---
+++ alembic/versions/d9f1e2a3b4c5_merge_survey_courses_and_exam_duration_heads.py
@@ -0,0 +1,23 @@
+"""merge_survey_courses_and_exam_duration_heads
+
+Revision ID: d9f1e2a3b4c5
+Revises: 1a2b3c4d5e7f, b7c9d1e2f3a4
+Create Date: 2026-04-01 12:30:00.000000
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "d9f1e2a3b4c5"
+down_revision = ("1a2b3c4d5e7f", "b7c9d1e2f3a4")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
